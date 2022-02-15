7bd7cead299dfd272800b39a6fdc07214d0c29c0
Carlos Ortiz <cortiz@thoughtworks.com>
2022-02-14 19:42:31 -0500

    Add Summary Form test and code

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,13 +1,24 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Popover from "react-bootstrap/Popover";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 
 export default function SummaryForm() {
   const [termsConditionsChecked, setTermsConditionsChecked] = useState(false);
 
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Body>No ice cream will actually be delivered</Popover.Body>
+    </Popover>
+  );
+
   const checkboxLabel = (
     <span>
-      I agree to <span style={{ color: 'blue' }}> Terms and Conditions</span>
+      I agree to
+      <OverlayTrigger placement="right" overlay={popover}>
+        <span style={{ color: "blue" }}> Terms and Conditions</span>
+      </OverlayTrigger>
     </span>
   );
 
@@ -21,7 +32,11 @@ export default function SummaryForm() {
           label={checkboxLabel}
         />
       </Form.Group>
-      <Button variant="primary" type="submit" disabled={!termsConditionsChecked}>
+      <Button
+        variant="primary"
+        type="submit"
+        disabled={!termsConditionsChecked}
+      >
         Confirm order
       </Button>
     </Form>
--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,5 +1,6 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen, waitForElementToBeRemoved } from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
+import userEvent from "@testing-library/user-event";
 
 describe("Confirm order button", () => {
   it("should be disabled by default", () => {
@@ -34,12 +35,39 @@ describe("Terms and Conditions checkbox", () => {
       name: /i agree to terms and conditions/i,
     });
 
-    fireEvent.click(termsConditionsCheckbox);
+    userEvent.click(termsConditionsCheckbox);
 
     expect(confirmOrderButton).toBeEnabled();
 
-    fireEvent.click(termsConditionsCheckbox);
+    userEvent.click(termsConditionsCheckbox);
 
     expect(confirmOrderButton).toBeDisabled();
+  });
+});
+
+describe("popover", () => {
+  it("should responds to hover", async() => {
+    render(<SummaryForm />);
+
+    // popover starts out hidden
+    const nullPopover = screen.queryByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(nullPopover).not.toBeInTheDocument();
+
+    // popover appears upon mouseover of checkbox label
+    const termsAndConditionsLabel = screen.getByText(/terms and conditions/i);
+    userEvent.hover(termsAndConditionsLabel);
+
+    const popover = screen.getByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(popover).toBeInTheDocument();
+
+    // popover disappears when we mouse out
+    userEvent.unhover(termsAndConditionsLabel);
+    await waitForElementToBeRemoved(() => screen.queryByText(
+      /no ice cream will actually be delivered/i
+    ));
   });
 });
